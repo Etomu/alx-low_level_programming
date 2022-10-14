e676fdcc2c195edafbea40b386e004e6e257ad5f
Etomu <etomutobi48@gmail.com>
2022-10-14 14:41:00 -0700

    T5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+	 * main - Entry point
+	 *
+	 * Description:  The main function serves as the starting
+	 * point for program execution.
+	 *
+	 * Return: Always 0 (Success)
+	 */
+int main(void)
+{
+printf("abcdfghijklmnoprstuvwxyz\n");
+return (0);
+}
