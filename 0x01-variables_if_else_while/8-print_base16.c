d7fd54a70a096cf7283378b3d422d62d8cf4613e
Etomu <etomutobi48@gmail.com>
2022-10-14 15:21:14 -0700

    T8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+	 * main - Entry point
+	 *
+	 * Description:  The main function serves as the starting
+	 * point for program execution.
+	 *
+	 * Return: Always 0 (Success)
+	 */
+int main(void)
+{
+char letter;
+
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar(letter);
+
+putchar('\n');
+
+return (0);
+}
