49241168a8a499f6457bf7b162d1deb5af150e25
tobi <113621482+Etomu@users.noreply.github.com>
2022-10-14 23:43:09 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,6 +13,14 @@
 	 */
 int main(void)
 {
-printf("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n");
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
+putchar('\n');
+
 return (0);
 }
