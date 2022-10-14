8796597422660c7eea2bc714340c42d765739479
Etomu <etomutobi48@gmail.com>
2022-10-14 12:40:56 -0700

    T0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,12 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+* Description: The main function is the
+* entry function
+**/
 int main(void)
 {
-/* Main is the entry function  */
 int n;
 
 srand(time(0));
