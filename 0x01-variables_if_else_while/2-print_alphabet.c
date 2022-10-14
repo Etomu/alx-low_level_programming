1cd31410553cfa094dec46b9012abb9bbd4c2e11
tobi <113621482+Etomu@users.noreply.github.com>
2022-10-14 23:42:24 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,6 +13,13 @@
 	 */
 int main(void)
 {
-printf("abcdefghijklmnopqrstuvwxyz\n");
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+
 return (0);
 }
