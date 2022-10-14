52903552c7a4d11ee28aa31129ef0d3124694fc4
Etomu <etomutobi48@gmail.com>
2022-10-14 15:27:33 -0700

    T10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+	 * main - Entry point
+	 *
+	 * Description:  The main function serves as the starting
+	 * point for program execution.
+	 *
+	 * Return: Always 0 (Success)
+	 */
+int main(void)
+{
+int num;
+
+for (num = 0; num <= 9; num++)
+{
+putchar((num % 10) + '0');
+if (num == 9)
+{
+continue;
+}
+	
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+}
