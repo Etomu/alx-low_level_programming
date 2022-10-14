cf360ad8f4424da6d0ed98e3a89504d652accab5
tobi <113621482+Etomu@users.noreply.github.com>
2022-10-14 23:23:11 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,11 +13,18 @@
 	 */
 int main(void)
 {
+int num;
 char letter;
 
-for (letter = 'z'; letter >= 'a'; letter--)
-putchar(letter);
+for (num = 0; num < 10; num++)
+{
+putchar((num % 10) + '0');
+}
 
+for (letter = 'a'; letter <= 'f'; letter++)
+{
+putchar(letter);
+}
 putchar('\n');
 
 return (0);
