a4cd97a4ff591a0e7764aff006efacdab61bc182
tobi <113621482+Etomu@users.noreply.github.com>
2022-10-14 22:00:03 +0100

    Update 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+	 * main - Entry point
+	 *
+	 * Description:  The main function serves as the starting
+	 * point for program execution.
+	 *
+	 * Return: Always 0 (Success)
+	 */
+int main(void)
+{
+
+return (0);
+}
