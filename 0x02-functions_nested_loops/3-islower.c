b16fb168cd9d3222e00b96358ca6b27604ed93fb
Etomu <etomutobi48@gmail.com>
2022-10-18 12:50:24 -0700

    T3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,35 @@
 #include "main.h"
 
+
+
 /**
-* _islower - checks for lowercase characters
-* @c: the number to be checked
-* Return: 1 if c is lowercase and 0 otherwise
-*/
+ *
+ * * _islower - checks for lowercase characters
+ *
+ * * @c: the number to be checked
+ *
+ * * Return: 1 if c is lowercase and 0 otherwise
+ *
+ * */
+
 int _islower(int c)
+
 {
-	if ((c >= 97) && (c <= 122))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+
+		if ((c >= 97) && (c <= 122))
+
+				{
+
+							return (1);
+
+								}
+
+			else
+
+					{
+
+								return (0);
+
+									}
+
 }
