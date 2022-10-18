1d387938e678634d5aa10eaccca6ab40bb7905cc
Etomu <etomutobi48@gmail.com>
2022-10-18 13:42:17 -0700

    T8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,26 +1,51 @@
 #include "main.h"
 
+
+
 /**
-* jack_bauer - prints every minute of the day Jack Bauer
-* Return: void
-*/
+ *
+ * * jack_bauer - prints every minute of the day Jack Bauer
+ *
+ * * Return: void
+ *
+ * */
+
 void jack_bauer(void)
 
-{
-int a;
 
-int b;
 
-for (a = 0; a <= 23; a++)
 {
-for (b = 0; b <= 59; b++)
-{
-_putchar (a / 10 + '0');
-_putchar (a % 10 + '0');
-_putchar (':');
-_putchar (b / 10 + '0');
-_putchar (b % 10 + '0');
-_putchar ('\n');
-}
-}
+
+	int a;
+
+
+
+	int b;
+
+
+
+	for (a = 0; a <= 23; a++)
+
+	{
+
+		for (b = 0; b <= 59; b++)
+
+		{
+
+			_putchar (a / 10 + '0');
+
+			_putchar (a % 10 + '0');
+
+			_putchar (':');
+
+			_putchar (b / 10 + '0');
+
+			_putchar (b % 10 + '0');
+
+			_putchar ('\n');
+
+		}
+
+	}
+
 }
