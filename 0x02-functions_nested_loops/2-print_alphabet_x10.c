ffea4b4cfc47d3c2a78cc81acedf0366ef3eb549
Etomu <etomutobi48@gmail.com>
2022-10-18 12:41:02 -0700

    T2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,43 @@
 #include "main.h"
 
+
+
 /**
-* print_alphabet_x10 - print the alphabets using _putchar
-* Description: prints lowercase letters a-z
-*/
+ *
+ * * print_alphabet_x10 - print the alphabets using _putchar
+ *
+ * * Description: prints lowercase letters a-z
+ *
+ * */
+
 void print_alphabet_x10(void)
+
 {
-	char letter;
 
-	int i;
+		char letter;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (letter = 'a'; letter <= 'z'; ++letter)
 
-			_putchar(letter);
 
-		_putchar('\n');
+			int i;
 
-	}
+
+
+				for (i = 0; i < 10; i++)
+
+						{
+
+									for (letter = 'a'; letter <= 'z'; ++letter)
+
+
+
+													_putchar(letter);
+
+
+
+											_putchar('\n');
+
+
+
+												}
+
 }
