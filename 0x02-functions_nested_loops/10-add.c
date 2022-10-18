7df1cc44d90fae310fe8da8709d8f1d8d658e291
Etomu <etomutobi48@gmail.com>
2022-10-18 13:58:41 -0700

    T10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,25 @@
 #include "main.h"
 
+
+
 /**
-* add - adds two integers
-* @i: first integer to add
-* @k: second integer to add
-* Return: the sum
-*/
+ *
+ * * add - adds two integers
+ *
+ * * @i: first integer to add
+ *
+ * * @k: second integer to add
+ *
+ * * Return: the sum
+ *
+ * */
+
 int add(int i, int k)
 
+
+
 {
-	return (i + k);
+
+		return (i + k);
+
 }
