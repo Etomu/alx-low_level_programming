6ae315c564aea4a60615839e741265631a933cdd
Etomu <etomutobi48@gmail.com>
2022-10-18 13:25:25 -0700

    T6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,20 +1,39 @@
 #include "main.h"
 
-/**
-* _abs - print absolute values of integers
-* @n: number to return absoluteness
-* Return: 0 for 0 and positive numbers and -1 for negative
-*/
-int _abs(int n)
-{
-	if (n >= 0)
-	{
-		return (n);
-	}
-	else
-	{
-		n = n * -1;
 
-		return (n);
-	}
+
+/**
+ *
+ * * _abs - print absolute values of integers
+ *
+ * * @n: number to return absoluteness
+ *
+ * * Return: 0 for 0 and positive numbers and -1 for negative
+ *
+ * */
+
+int _abs(int n)
+
+{
+
+		if (n >= 0)
+
+				{
+
+							return (n);
+
+								}
+
+			else
+
+					{
+
+								n = n * -1;
+
+
+
+										return (n);
+
+											}
+
 }
