92f5adba2f2bb86783e468ea39117041dd8dd21c
tobi <113621482+Etomu@users.noreply.github.com>
2022-10-18 18:33:23 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,39 +1,21 @@
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- *  * _putchar - writes the character c to stdout
- *
- *   * @c: The character to print
- *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately.
- *
- *       */
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 void print_times_table(int n);
-
 void print_to_98(int n);
-
 int add(int, int);
-
 void times_table(void);
-
 void jack_bauer(void);
-
 int print_last_digit(int);
-
 int _abs(int);
-
 int print_sign(int n);
-
 void print_alphabet(void);
-
 void print_alphabet_x10(void);
-
 int _islower(int c);
-
 int _isalpha(int c);
-
 int _putchar(char c);
