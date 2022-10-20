267bc3a563b32fca0410c973d1136509c0a7304c
tobi <113621482+Etomu@users.noreply.github.com>
2022-10-20 19:59:45 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,55 +1,28 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - prints the fizz buzz challenge
- *
- *   * Return: 0 always
- *
- *   */
-
-
+ * main - prints the fizz buzz challenge
+ * Return: 0 always
+ */
 
 int main(void)
-
 {
+	int i = 1;
 
-		int i = 1;
-
-
-
-			while (i <= 100)
-
-					{
-
-								if (i % 3 == 0 && i % 5 == 0)
-
-												printf("FizzBuzz ");
-
-										else if	(i % 3 == 0)
-
-														printf("Fizz ");
-
-												else if (i % 5 == 0)
-
-																if (i == 100)
-
-																					printf("Buzz\n");
-
-															else
-
-																				printf("Buzz ");
-
-														else
-
-																		printf("%d ", i);
-
-																i++;
-
-																	}
-
-				return (0);
-
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if	(i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			if (i == 100)
+				printf("Buzz\n");
+			else
+				printf("Buzz ");
+		else
+			printf("%d ", i);
+		i++;
+	}
+	return (0);
 }
