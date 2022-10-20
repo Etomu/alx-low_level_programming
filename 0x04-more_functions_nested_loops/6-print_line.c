54c911ba843d3830a0234bae2d0c415ee6bff9e6
Etomu <etomutobi48@gmail.com>
2022-10-20 11:17:48 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+
+
+/**
+ *
+ * * print_line - print a line with a \n
+ *
+ * * @n: number of underscores to print
+ *
+ * * Return: void
+ *
+ * */
+
+
+
+void print_line(int n)
+
+{
+
+		int i = 0;
+
+
+
+			if (n <= 0)
+
+						_putchar('\n');
+
+				else
+
+						{
+
+									while (i < n)
+
+												{
+
+																_putchar('_');
+
+																			i++;
+
+																					}
+
+											_putchar('\n');
+
+												}
+
+}
