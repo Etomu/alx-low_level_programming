05d05872ba54728ac3c720fbcdfa72723812e0ce
tobi <113621482+Etomu@users.noreply.github.com>
2022-10-20 19:34:36 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,47 +1,24 @@
 #include "main.h"
 
-
-
 /**
- *
- * * print_line - print a line with a \n
- *
- * * @n: number of underscores to print
- *
- * * Return: void
- *
- * */
-
-
+ * print_line - print a line with a \n
+ * @n: number of underscores to print
+ * Return: void
+ */
 
 void print_line(int n)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			if (n <= 0)
-
-						_putchar('\n');
-
-				else
-
-						{
-
-									while (i < n)
-
-												{
-
-																_putchar('_');
-
-																			i++;
-
-																					}
-
-											_putchar('\n');
-
-												}
-
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
 }
