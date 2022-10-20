4262d0ce181f50b825446f95f77f0040432d99df
Etomu <etomutobi48@gmail.com>
2022-10-20 11:43:58 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_square - prints a square using # sign
+ *
+ *   * @size: size of the square
+ *
+ *    * Return: void
+ *
+ *    */
+
+
+
+void print_square(int size)
+
+{
+
+		int i = 0;
+
+			int j;
+
+
+
+				if (size <= 0)
+
+							_putchar('\n');
+
+					else
+
+							{
+
+										while (i < size)
+
+													{
+
+																	for (j = 0; j < size; j++)
+
+																						_putchar('#');
+
+																				_putchar('\n');
+
+																							i++;
+
+																									}
+
+											}
+
+}
