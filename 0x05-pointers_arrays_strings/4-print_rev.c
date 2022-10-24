d6a0dc04ff0a116aeb40eccb5aa3dc397e592803
tobi <113621482+Etomu@users.noreply.github.com>
2022-10-24 14:19:48 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,43 +1,22 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * print_rev - prints a string in reverse order
- *
- *   * @s: string to be reversed
- *
- *    * Return: void
- *
- *     */
-
-
+ * print_rev - prints a string in reverse order
+ * @s: string to be reversed
+ * Return: void
+ */
 
 void print_rev(char *s)
-
 {
+	int len = 0;
 
-		int len = 0;
-
-
-
-			while (s[len] != '\0')
-
-						{
-
-									len++;
-
-											}
-
-				while (len)
-
-							{
-
-										_putchar(s[--len]);
-
-												}
-
-					_putchar('\n');
-
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	while (len)
+	{
+		_putchar(s[--len]);
+	}
+	_putchar('\n');
 }
