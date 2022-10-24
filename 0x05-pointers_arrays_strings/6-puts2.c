fbe27adfc2f3b39d29034da25be58431c38a4599
Etomu <etomutobi48@gmail.com>
2022-10-24 06:30:18 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * puts2 - prints every other character of a string
+ *
+ *   * @str: string to puts2
+ *
+ *    * Return: void
+ *
+ *     */
+
+
+
+void puts2(char *str)
+
+{
+
+		int len = 0;
+
+
+
+			while (str[len] != '\0')
+
+					{
+
+								if (len % 2 == 0)
+
+												_putchar(str[len]);
+
+										len++;
+
+											}
+
+				_putchar('\n');
+
+}
