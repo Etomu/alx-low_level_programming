7837439879b0152b8e87a9a4121414c0eaa0a315
tobi <113621482+Etomu@users.noreply.github.com>
2022-10-26 17:18:20 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,55 +1,28 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
- *
- *   * print_number - print an integer
- *
- *     * @n: integer params
- *
- *       *  Return: 0
- *
- *        **/
-
-
+ * print_number - print an integer
+ * @n: integer params
+ *  Return: 0
+**/
 
 void print_number(int n)
 
-
-
 {
 
+unsigned int n1;
 
+n1 = n;
 
-	unsigned int n1;
+if (n < 0)
+{
+_putchar('-');
 
-
-
-	n1 = n;
-
-
-
-	if (n < 0)
-
-	{
-
-		_putchar('-');
-
-
-
-		n1 = -n;
-
-	}
-
-	if (n1 / 10 != 0)
-
-	{
-
-		print_number(n1 / 10);
-
-	}
-
-	_putchar((n1 % 10) + '0');
-
+n1 = -n;
+}
+if (n1 / 10 != 0)
+{
+print_number(n1 / 10);
+}
+_putchar((n1 % 10) + '0');
 }
