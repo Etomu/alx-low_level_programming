fe8b0b33dbffe8e9623afd9aa131b6278b7e49b5
tobi <113621482+Etomu@users.noreply.github.com>
2022-10-26 19:13:46 +0100

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,30 +3,30 @@
 int main(void)
 {
 
-	int n;
+  int n;
 
-	int a[5];
+  int a[5];
 
-	int *p;
+  int *p;
 
-	a[2] = 1024;
+  a[2] = 1024;
 
-	p = &n;
+  p = &n;
 
-/*
- * write your line of code here...
- * Remember:
- * - you are not allowed to use variable a
- * - you are not allowed to modify variable p
- * - only one statement
- * - you are not allowed to code anything else than this line of code
- */
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use variable a
+   * - you are not allowed to modify variable p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
 
-*(p + 5) = 98;
+  *(p + 5) = 98;
 
-/* ...so that this prints 98\n */
+  /* ...so that this prints 98\n */
 
-printf("a[2] = %d\n", a[2]);
+  printf("a[2] = %d\n", a[2]);
 
-return (0);
+  return (0);
 }
