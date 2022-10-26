d7005d55351e3688b14621efe5a6332202e2b6c6
tobi <113621482+Etomu@users.noreply.github.com>
2022-10-26 19:11:51 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,13 @@
 int check_seperators(char c);
 
 /**
- * main: cap_string - function to capitalise all words of a string.
- * Description: a function that capitalises all words of a string.
- * Prototype: char *cap_string(char *);
- * Separators of words: space, tabulation,
- * new line, ,, ;, ., !, ?, ''(actually double quote), (, ), {, and }
- * @s: An input string to capitalise letters
+ * cap_string - a function that capitalizes all words of a string.
+ * @s: An input string to capitalize letters
  * Return: pointer to s
  */
 
 char *cap_string(char *s)
-{
-	int i = 0;
+{	int i = 0;
 
 	while (s[i])
 	{
@@ -20,7 +15,7 @@ char *cap_string(char *s)
 
 			s[i] -= 32;
 
-		if (check_separators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 
 			s[i + 1] -= 32;
 
@@ -31,22 +26,23 @@ char *cap_string(char *s)
 }
 
 /**
- * check_separators - Separators of words: space, tabulation, new line,
- * ,, ;, ., !, ?, ", (, ), {, and }
- * @c: an input character
- * Return: 1 if separator, 0 otherwise
+ *  check_seperators - Separators of words: space, tabulation, new line,
+ *  ,, ;, ., !, ?, ", (, ), {, and }
+ *  @c: an input character
+ *  Return: 1 if seperator, 0 otherwise
  */
 
-int check_separators(char c)
+int check_seperators(char c)
 {
 	int i = 0;
 
-	char separators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+
 		'"', '(', ')', '{', '}' };
 
 	for (; i < 13; i++)
 	{
-		if (c == separators[i])
+		if (c == seperators[i])
 
 			return (1);
 	}
