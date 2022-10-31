8a4622df773f585749424a2d490f1c400834a8e0
tobi <113621482+Etomu@users.noreply.github.com>
2022-10-31 18:53:18 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,51 +1,27 @@
 #include "main.h"
 
-
-
 /**
- *
- * * main: _strchr - search for a character
- *
- * * @s: string to search
- *
- * * @c: character to search for
- *
- * * Return: pointer to the first occurrence of c
- *
- * */
-
-
+ * main: _strchr - search for a character
+ * @s: string to search
+ * @c: character to search for
+ * Return: pointer to the first occurrence of c
+ */
 
 char *_strchr(char *s, char c)
-
 {
+	int i;
 
-		int i;
+	char *n;
 
-			char *n;
-
-
-
-				for (i = 0; *(s + i) >= '\0'; i++)
-
-						{
-
-									if (*(s + i) == c)
-
-												{
-
-																n = s + i;
-
-																			break;
-
-																					}
-
-											else
-
-															n = '\0';
-
-												}
-
-					return (n);
-
+	for (i = 0; *(s + i) >= '\0'; i++)
+	{
+		if (*(s + i) == c)
+		{
+			n = s + i;
+			break;
+		}
+		else
+			n = '\0';
+	}
+	return (n);
 }
