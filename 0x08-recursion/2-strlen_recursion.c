bd18014efd9c4b6b877f13f536ac50d71fa0ebed
tobi <113621482+Etomu@users.noreply.github.com>
2022-11-02 19:24:04 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,33 +1,16 @@
 #include "main.h"
 
-
-
 /**
- *
- * * _strlen_recursion - add up length of string recursively
- *
- * * @s: string to count
- *
- * * Return: length of string
- *
- * */
-
-
+ * _strlen_recursion - add up length of string recursively
+ * @s: string to count
+ * Return: length of string
+ */
 
 int _strlen_recursion(char *s)
-
-
-
 {
-
-		if (*s)
-
-				{
-
-							return (1 + _strlen_recursion(s + 1));
-
-								}
-
-			return (0);
-
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
 }
