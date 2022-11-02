50e377002d767d8fe843e199264c4c091b0d2df2
tobi <113621482+Etomu@users.noreply.github.com>
2022-11-02 19:28:58 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,34 +1,18 @@
 #include "main.h"
 
-
-
 /**
- *
- * * _puts_recursion - prints out a string with recursion
- *
- * * @s: string to print out
- *
- * * Return: void
- *
- * */
-
-
+ * _puts_recursion - prints out a string with recursion
+ * @s: string to print out
+ * Return: void
+ */
 
 void _puts_recursion(char *s)
-
 {
-
-		if (*s)
-
-				{
-
-							_putchar(*s);
-
-									_puts_recursion(s + 1);
-
-										}
-
-			else
-
-						_putchar('\n');
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
