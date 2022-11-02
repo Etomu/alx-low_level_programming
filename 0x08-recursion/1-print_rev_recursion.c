dca485ae3088ed3d76b6572a5c1adfd8ea54fae8
Etomu <etomutobi48@gmail.com>
2022-11-02 11:02:32 -0700

    0x08. C - Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+ *
+ * * _print_rev_recursion - print strings in reverse order
+ *
+ * * @s: string to reverse
+ *
+ * * Return: void
+ *
+ * */
+
+
+
+void _print_rev_recursion(char *s)
+
+
+
+{
+
+		if (*s)
+
+				{
+
+							_print_rev_recursion(s + 1);
+
+									_putchar(*s);
+
+										}
+
+}
