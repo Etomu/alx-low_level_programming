3dae02561c5859a7137fffa3ade0c50e4bd29704
tobi <113621482+Etomu@users.noreply.github.com>
2022-11-16 18:53:55 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -11,32 +11,38 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
  * op_sub - subtracts two numbers
  * @a: first number
  * @b: second number
  * Return: the subtracted value
 */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
  * op_mul - multiplies two numbers
  * @a: first number
  * @b: second number
  * Return: the multiplied value
 */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
  * op_div - divides two numbers
  * @a: first number
  * @b: second number
  * Return: the divided value
 */
+
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -46,12 +52,14 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
+
 /**
  * op_mod - adds two numbers
  * @a: first number
  * @b: second number
  * Return: the added sum
 */
+
 int op_mod(int a, int b)
 {
 
