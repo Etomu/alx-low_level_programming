016d2f4b4f5b0ac7b4791df6987d18e2472d8e24
tobi <113621482+Etomu@users.noreply.github.com>
2022-11-16 18:48:52 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,60 +1,64 @@
 #include "3-calc.h"
 
 /**
-* op_add - adds two ints
-* @a: number 1
-* @b: number 2
-* Return: sum of the two numbers
-**/
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the added sum
+*/
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
-* op_sub - subtract two numbers
-* @a: number 1
-* @b: number 2
-* Return: result of substraction
-**/
+ * op_sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the subtracted value
+*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
-* op_mul - multiply two numbers
-* @a: number 1
-* @b: number 2
-* Return: result of multiplication
-**/
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the multiplied value
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
-* op_div - divides two numbers
-* @a: number 1
-* @b: number 2
-* Return: result of divison
-**/
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the divided value
+*/
 int op_div(int a, int b)
 {
 	if (b == 0)
-		return (-1);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
-
 /**
-* op_mod - calculates mod of two ints
-* @a: number 1
-* @b: number 2
-* Return: mod of 1, 2
-**/
+ * op_mod - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the added sum
+*/
 int op_mod(int a, int b)
 {
+
 	if (b == 0)
-		return (-1);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
