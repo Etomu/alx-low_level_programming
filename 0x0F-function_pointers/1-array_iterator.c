1d8e373539ebcaa19209b00a887b1a740580a46d
tobi <113621482+Etomu@users.noreply.github.com>
2022-11-16 18:27:03 +0100

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,11 +7,12 @@
  * @size: size of array
  * @action: pointer to address of function to execute
  * Return: void
- */
+*/
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
+
 	if (array && action)
 	{
 		for (i = 0; i < size; i++)
