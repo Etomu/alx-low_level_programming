1aaccf850c9578fd957d53b5f381a08372d0a16e
tobi <113621482+Etomu@users.noreply.github.com>
2022-11-16 18:43:34 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
-
 /**
- * main - check the code for Holberton School students.
- * @argc: argument count.
- * @argv: argument vector.
- * Return: Always 0.
+ * main - call functions to print answer
+ * @argc: num of arg
+ * @argv: atguments
+ * Return: 0 or errors
  */
-int main(int argc, char *argv[])
-
+int main(int argc, char **argv)
 {
-	int a, b;
-	int (*operation)(int, int);
+	char aux = argv[2][0];
+	int a = 0, num1 = 0, num2 = 0;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	
-	if (argv[2][1])
+	if (aux != '+' && aux != '-' && aux != '*'
+	     && aux != '/' && aux != '%')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	
-	operation = get_op_func(argv[2]);
-	
-	if (operation == NULL)
+	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	
-	printf("%d\n", operation(a, b));
 
+	 num1 = atoi(argv[1]);
+	 num2 = atoi(argv[3]);
+
+	a = (get_op_func(argv[2]))(num1, num2);
+
+	printf("%d\n", a);
 	return (0);
 }
