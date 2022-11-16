3cfec19a199fad60ff61069475fdb459253bca57
Etomu <etomutobi48@gmail.com>
2022-11-16 09:59:33 -0800

    task1

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,42 +1,83 @@
 #include "3-calc.h"
 
+
+
 /**
- * main - check the code for Holberton School students.
- * @argc: argument count.
- * @argv: argument vector.
- * Return: Always 0.
- */
+ *
+ *  * main - check the code for Holberton School students.
+ *
+ *   * @argc: argument count.
+ *
+ *    * @argv: argument vector.
+ *
+ *     * Return: Always 0.
+ *
+ *      */
+
+
 
 int main(int argc, char *argv[])
 
+
+
 {
-	int a, b;
-	int (*operation)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		int a, b;
 
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(99);
-	}
+			int (*operation)(int, int);
 
-	operation = get_op_func(argv[2]);
-	
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
 
-	printf("%d\n", operation(a, b));
+				if (argc != 4)
 
-	return (0);
+						{
+
+									printf("Error\n");
+
+											exit(98);
+
+												}
+
+
+
+					if (argv[2][1])
+
+							{
+
+										printf("Error\n");
+
+												exit(99);
+
+													}
+
+
+
+						operation = get_op_func(argv[2]);
+
+							
+
+							if (operation == NULL)
+
+									{
+
+												printf("Error\n");
+
+														exit(99);
+
+															}
+
+
+
+								a = atoi(argv[1]);
+
+									b = atoi(argv[3]);
+
+
+
+										printf("%d\n", operation(a, b));
+
+
+
+											return (0);
+
 }
