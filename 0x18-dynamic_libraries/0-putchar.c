022008171e86aeacd9fc3c9344f262cd87da238d
Etomu <etomutobi48@gmail.com>
2023-01-16 14:03:08 -0800

    0x18. C - Dynamic libraries

---
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always success
+ *
+ *     */
+
+int main(void)
+
+{
+
+
+
+	printf("_putchar\n");
+
+
+
+	return (0);
+
+}
